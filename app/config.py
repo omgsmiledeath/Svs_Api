7520ac4e60cc3102a3bf061ac5e4168bac433d79
OmgSmileDeath <araragikyn@gmail.com>
2023-09-07 14:37:44 +0800

    comments #

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__)) #дл опредления местоположения
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, 'app.db')  #проверка на наличае файла , либо его создание
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- app/models.py
+++ app/models.py
@@ -1,15 +1,15 @@
-from app import db
+from app import db # импорт из папки app 
 
-class Entry(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
+class Entry(db.Model): #сущность Записи
+    id = db.Column(db.Integer,primary_key=True)  #поля таблицы
     date = db.Column(db.DateTime, nullable=False)
     owner = db.Column(db.String(20),nullable=False)
     desc = db.Column(db.String(120),nullable=True)
 
-    def __repr__(self):
+    def __repr__(self):   #для печати класса в стандартном виде (аля toString())
         return '<Entry {}>'.format(self.date) 
    
-class User(db.Model):
+class User(db.Model): #Сущность Юзеры
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(20))
     password = db.Column(db.String)
