4aa45f0a3ede59e106fc81abd135879d8fd24e71
OmgSmileDeath <araragikyn@gmail.com>
2023-09-08 21:18:09 +0800

    post method code

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!flask/bin/python
 import sqlite3
-from flask import Flask,jsonify,make_response  #Импорт методов из библиотеки Flask
+import json
+from flask import Flask,jsonify,make_response,request  #Импорт методов из библиотеки Flask
 
 
 app = Flask(__name__)  #Создаем само приложение
@@ -24,15 +25,25 @@ def get_entries():
     except Exception as ex:
         return ex.__str__()
 
-@app.route('api/v1/entries',methods=['GET'])
-def add_entries(entries):
+@app.route('/api/v1/newentry',methods=['POST'])
+def add_entries():
+    if not request.json:
+        abort(400)
     try:
+        temp = json.loads(request.json)
+        print(temp) 
         con = sqlite3.connect("./app/app.db")
         cur = con.cursor()
-        cur.execute()
+        resSql = cur.execute("SELECT * FROM ENTRY")
+        res = resSql.fetchall()
+        print(res)
         response = make_response(
-            jsonify(entries,201)
-        )
+            jsonify(
+                res),
+                201,
+                )
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
     except Exception as ex:
         return ex.__str__()
 if __name__== '__main__':
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import sqlite3
-
+from datetime import datetime
 
 con = sqlite3.connect("./app/app.db")
 cur = con.cursor()
 
-cur.execute("INSERT INTO entry (date,owner,desc) values ('2023-09-07-12-00-00','я','железо')")
+cur.execute("INSERT INTO entry(date,owner,desc) value('')")
 
 con.commit()
