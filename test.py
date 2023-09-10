6b7809cdc2ec9a711474d9518f8a8eb9e27bc6fe
OmgSmileDeath <araragikyn@gmail.com>
2023-09-10 17:29:16 +0800

    clean db

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ con = sqlite3.connect("./app/app.db")
 cur = con.cursor()
 
 #cur.execute("INSERT INTO user(name,password,token) VALUES ('Admin','Admin','token')")
-for i in range(1,16):
+for i in range(16,32):
     cur.execute(f"DELETE FROM entry WHERE id={i}")
-    con.commit()
+con.commit()
 con.commit()
